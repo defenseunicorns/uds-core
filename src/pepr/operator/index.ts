3e492e3f9b04c2c82af2b070ea844adc82d186cd
Jeff McCoy <code@jeffm.us>
2024-01-03 03:21:28 -0600

    create separate reconciler file + use PathStatus() for status updates

--- src/pepr/operator/index.ts
+++ src/pepr/operator/index.ts
@@ -1,12 +1,9 @@
-import { Capability, Log } from "pepr";
+import { Capability } from "pepr";
 
-import { UDSConfig } from "../config";
 import { UDSPackage } from "./crd";
 import "./crd/register";
 import { validator } from "./crd/validator";
-import { virtualService } from "./istio";
-import { syncNamespace } from "./namespace";
-import { networkPolicies } from "./network";
+import { Queue } from "./enque";
 
 export const operator = new Capability({
   name: "uds-core-operator",
@@ -15,33 +12,11 @@ export const operator = new Capability({
 
 export const { Store, When } = operator;
 
+const queue = new Queue();
+
 When(UDSPackage)
   .IsCreatedOrUpdated()
+  // Advanced CR validation
   .Validate(validator)
-  .Watch(async pkg => {
-    if (!pkg.metadata?.namespace) {
-      Log.error(pkg, `Invalid Package definition`);
-      return;
-    }
-
-    Log.debug(pkg, `Processing Package ${pkg.metadata.namespace}/${pkg.metadata.name}`);
-
-    // Configure the namespace and namespace-wide network policies
-    try {
-      const namespace = await syncNamespace(pkg);
-
-      await networkPolicies(pkg, namespace);
-
-      // Only configure the VirtualService if Istio is installed
-      if (UDSConfig.istioInstalled) {
-        await virtualService(pkg, namespace);
-      } else {
-        Log.warn(`Istio is not installed, skipping ${pkg.metadata.name} VirtualService.`);
-      }
-    } catch (e) {
-      Log.error(
-        e,
-        `Error completing configuration for ${pkg.metadata.namespace}/${pkg.metadata.name}`,
-      );
-    }
-  });
+  // Enqueue the package for processing
+  .Watch(pkg => queue.enqueue(pkg));
---
+++ src/pepr/operator/reconciler.ts
@@ -0,0 +1,74 @@
+import { K8s, Log } from "pepr";
+
+import { UDSConfig } from "../config";
+import { virtualService } from "./controllers/istio";
+import { syncNamespace } from "./controllers/namespace";
+import { networkPolicies } from "./controllers/network";
+import { Phase, Status, UDSPackage } from "./crd";
+import { VirtualService } from "./crd/generated/istio/virtualservice-v1beta1";
+
+/**
+ * The reconciler is called from the queue and is responsible for reconciling the state of the package
+ * with the cluster. This includes creating the namespace, network policies and virtual services.
+ *
+ * @param pkg the package to reconcile
+ */
+export async function reconciler(pkg: UDSPackage) {
+  if (!pkg.metadata?.namespace) {
+    Log.error(pkg, `Invalid Package definition`);
+    return;
+  }
+
+  const isPending = pkg.status?.phase === Phase.Pending;
+  const isCurrentGeneration = pkg.metadata.generation === pkg.status?.observedGeneration;
+
+  if (isPending || isCurrentGeneration) {
+    Log.debug(pkg, `Skipping pending or completed package`);
+    return;
+  }
+
+  Log.debug(pkg, `Processing Package ${pkg.metadata.namespace}/${pkg.metadata.name}`);
+
+  // Configure the namespace and namespace-wide network policies
+  try {
+    void updateStatus(pkg, { phase: Phase.Pending });
+
+    const namespace = await syncNamespace(pkg);
+
+    const netPol = await networkPolicies(pkg, namespace);
+
+    // Only configure the VirtualService if Istio is installed
+    let vs: VirtualService[] = [];
+    if (UDSConfig.istioInstalled) {
+      vs = await virtualService(pkg, namespace);
+    } else {
+      Log.warn(`Istio is not installed, skipping ${pkg.metadata.name} VirtualService.`);
+    }
+
+    void updateStatus(pkg, {
+      phase: Phase.Ready,
+      endpoints: vs.map(v => v.spec!.hosts!.join(",")),
+      networkPolicyCount: netPol.length,
+      observedGeneration: pkg.metadata.generation,
+    });
+  } catch (e) {
+    Log.error(e, `Error configuring for ${pkg.metadata.namespace}/${pkg.metadata.name}`);
+    void updateStatus(pkg, { phase: Phase.Failed });
+  }
+}
+
+/**
+ * Updates the status of the package
+ *
+ * @param pkg The package to update
+ * @param status The new status
+ */
+async function updateStatus(pkg: UDSPackage, status: Status) {
+  await K8s(UDSPackage).PatchStatus({
+    metadata: {
+      name: pkg.metadata!.name,
+      namespace: pkg.metadata!.namespace,
+    },
+    status,
+  });
+}
