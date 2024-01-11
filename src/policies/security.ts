88b969e2aa4dea8b76dbe397d77c53941f7cfbc8
Tristan Holaday <40547442+TristanHoladay@users.noreply.github.com>
2024-01-11 11:03:28 -0700

    fix: add test for disallow selinux options and handle checking for us… (#96)
    ## Description
    Handling when seLinuxOptions contains defined user or role fields.
    
    ## Related Issue
    Fixes #95
    
    ## Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [x] Test, docs, adr added or updated as needed
    - [ ] [Contributor Guide
    Steps](https://github.com/defenseunicorns/uds-template-capability/blob/main/CONTRIBUTING.md)(https://github.com/defenseunicorns/uds-template-capability/blob/main/CONTRIBUTING.md#submitting-a-pull-request)
    followed

--- src/policies/security.ts
+++ src/policies/security.ts
@@ -201,6 +201,49 @@ When(a.Pod)
   });
 
 /**
+ * Disallow SELinux Options in Pods
+ *
+ * SELinux options can be used to escalate privileges. This policy ensures that the
+ * `seLinuxOptions` user and role fields are set to undefined.
+ * Applies to Pods and all types of containers within them.
+ *
+ * @related https://repo1.dso.mil/big-bang/product/packages/kyverno-policies/-/blob/main/chart/templates/disallow-selinux-options.yaml
+ */
+When(a.Pod)
+  .IsCreatedOrUpdated()
+  .Validate(request => {
+    const seLinuxOptions = request.Raw.spec?.securityContext?.seLinuxOptions;
+    const authorized = ['user: undefined', 'role: undefined']
+
+    // Check Pod level security context
+    if(seLinuxOptions?.user || seLinuxOptions?.role) {
+       return request.Deny(
+         securityContextMessage(`Unauthorized pod SELinux Options`, authorized, [
+           { ctx: request.Raw.spec?.securityContext as V1SecurityContext },
+         ]),
+       );
+    }
+
+    // Check Container level security context
+    const violations = securityContextContainers(request).filter(
+      c => c.ctx.seLinuxOptions?.user || c.ctx.seLinuxOptions?.role
+    );
+
+
+    if (violations.length) {
+      return request.Deny(
+        securityContextMessage(
+          "Unauthorized container SELinux Options",
+          authorized,
+          violations,
+        ),
+      );
+    }
+
+    return request.Approve();
+  });
+
+/**
  * Restrict SELinux Type in Pods
  *
  * SELinux options can be used to escalate privileges. This policy ensures that the
