543b09d103a43c474da6a8c950404cc1f373b03f
Tristan Holaday <40547442+TristanHoladay@users.noreply.github.com>
2024-04-03 09:32:43 -0600

    feat: add nightly testing eks (#250)
    ## Description
    Add EKS setup for deploying and testing uds-core.
    
    ## Related Issue
    related to #213 
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [ ] Test, docs, adr added or updated as needed
    - [ ] [Contributor Guide
    Steps](https://github.com/defenseunicorns/uds-template-capability/blob/main/CONTRIBUTING.md)(https://github.com/defenseunicorns/uds-template-capability/blob/main/CONTRIBUTING.md#submitting-a-pull-request)
    followed
    
    ---------
    
    Co-authored-by: zamaz <71521611+zachariahmiller@users.noreply.github.com>

---
+++ .github/test-infra/buckets-iac/velero.tf
@@ -0,0 +1,59 @@
+resource "aws_iam_policy" "velero_policy" {
+  name        = "${local.bucket_configurations.velero.name}-irsa-${random_id.unique_id.hex}"
+  path        = "/"
+  description = "Policy to give Velero necessary permissions for cluster backups."
+
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect = "Allow",
+          Action = [
+            "ec2:DescribeVolumes",
+            "ec2:DescribeSnapshots",
+            "ec2:CreateTags",
+            "ec2:CreateVolume",
+            "ec2:CreateSnapshot",
+            "ec2:DeleteSnapshot"
+          ]
+          Resource = [
+            "*"
+          ]
+        },
+        {
+          Effect = "Allow"
+          Action = [
+            "s3:GetObject",
+            "s3:DeleteObject",
+            "s3:PutObject",
+            "s3:AbortMultipartUpload",
+            "s3:ListMultipartUploadParts"
+          ]
+          Resource = [
+            "arn:${data.aws_partition.current.partition}:s3:::${module.S3["velero"].bucket_name}/*"
+          ]
+        },
+        {
+          Effect = "Allow",
+          Action = [
+            "s3:ListBucket"
+          ],
+          Resource = [
+            "arn:${data.aws_partition.current.partition}:s3:::${module.S3["velero"].bucket_name}/*"
+          ]
+        },
+        {
+          Effect = "Allow"
+          Action = [
+            "kms:GenerateDataKey",
+            "kms:Decrypt"
+          ]
+          Resource = [local.kms_key_arns["velero"].kms_key_arn]
+        }
+
+      ]
+  })
+}
+
