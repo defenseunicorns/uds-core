3a96e11355852ff56925f71ca8418de872cff9ee
Eric Wyles <23637493+ericwyles@users.noreply.github.com>
2024-04-16 13:32:35 -0500

    removed protocol mappers

--- src/pepr/operator/crd/generated/package-v1alpha1.ts
+++ src/pepr/operator/crd/generated/package-v1alpha1.ts
@@ -458,10 +458,6 @@ export interface Sso {
    */
   protocol?: Protocol;
   /**
-   * Protocol Mappers to configure on the client
-   */
-  protocolMappers?: ProtocolMapper[];
-  /**
    * Valid URI pattern a browser can redirect to after a successful login. Simple wildcards
    * are allowed such as 'https://unicorns.uds.dev/*'
    */
@@ -503,25 +499,6 @@ export enum ClientAuthenticatorType {
 export enum Protocol {
   OpenidConnect = "openid-connect",
   Saml = "saml",
-}
-
-export interface ProtocolMapper {
-  /**
-   * Configuration options for the mapper.
-   */
-  config?: { [key: string]: string };
-  /**
-   * Name of the mapper
-   */
-  name?: string;
-  /**
-   * Protocol of the mapper
-   */
-  protocol?: string;
-  /**
-   * Protocol Mapper type of the mapper
-   */
-  protocolMapper?: string;
 }
 
 export interface Status {
