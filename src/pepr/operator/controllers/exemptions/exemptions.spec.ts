b56875a5b6c4ec618876a8589565fd68b82fb54b
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2024-05-15 11:31:29 -0600

    pepr format

--- src/pepr/operator/controllers/exemptions/exemptions.spec.ts
+++ src/pepr/operator/controllers/exemptions/exemptions.spec.ts
@@ -125,300 +125,277 @@ describe("Test await processExemptions()", () => {
     ]);
   });
 
-    it("Removes exemptions from policy if policies removed from matcher policy list on update", async () => {
-      const updatedNeuvectorExemption = {
-        metadata: {
-          uid: exemption1UID,
-        },
-        spec: {
-          exemptions: [
-            { matcher: enforcerMatcher, policies: [Policy.DisallowPrivileged] },
-            {
-              matcher: controllerMatcher,
-              policies: [Policy.DropAllCapabilities],
-            },
-            {
-              matcher: prometheusMatcher,
-              policies: [Policy.DropAllCapabilities],
-            },
-          ],
-        },
-      } as Exemption;
+  it("Removes exemptions from policy if policies removed from matcher policy list on update", async () => {
+    const updatedNeuvectorExemption = {
+      metadata: {
+        uid: exemption1UID,
+      },
+      spec: {
+        exemptions: [
+          { matcher: enforcerMatcher, policies: [Policy.DisallowPrivileged] },
+          {
+            matcher: controllerMatcher,
+            policies: [Policy.DropAllCapabilities],
+          },
+          {
+            matcher: prometheusMatcher,
+            policies: [Policy.DropAllCapabilities],
+          },
+        ],
+      },
+    } as Exemption;
 
-      processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
-      processExemptions(updatedNeuvectorExemption, "MODIFIED", exemptionMap);
+    processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
+    processExemptions(updatedNeuvectorExemption, "MODIFIED", exemptionMap);
 
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([]);
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedControllerMatcher,storedPrometheusMatcher],
-      );
-    });
-
-    it("Removes matchers from policy if matchers removed from CR", async () => {
-      const updatedNeuvectorExemption = {
-        metadata: {
-          uid: exemption1UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: controllerMatcher,
-              policies: [Policy.DisallowPrivileged, Policy.DropAllCapabilities],
-            },
-            {
-              matcher: { ...enforcerMatcher, kind: MatcherKind.Service },
-              policies: [Policy.DisallowNodePortServices],
-            },
-          ],
-        },
-      } as Exemption;
-
-      processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
-      processExemptions(updatedNeuvectorExemption, "MODIFIED", exemptionMap);
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([]);
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedControllerMatcher],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedControllerMatcher],
-      );
-      expect(exemptionMap.get(Policy.DisallowNodePortServices)).toEqual(
-        [{ ...storedEnforcerMatcher, kind: MatcherKind.Service }],
-      );
-    });
-
-    it("Adds duplicate exemptions set by same CR if different matcher kind", async () => {
-      const neuvectorMockExemption2 = {
-        metadata: {
-          uid: exemption1UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: enforcerMatcher,
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-            {
-              matcher: { ...enforcerMatcher, kind: MatcherKind.Service },
-              policies: [Policy.DisallowNodePortServices],
-            },
-          ],
-        },
-      } as Exemption;
-
-      processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
-
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.DisallowNodePortServices)).toEqual(
-        [{ ...storedEnforcerMatcher, kind: MatcherKind.Service }],
-      );
-    });
-
-    it("Adds duplicate exemptions set by same CR if different namespace", async () => {
-      const diffNS = "differentNS";
-      const neuvectorMockExemption2 = {
-        metadata: {
-          uid: exemption1UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: enforcerMatcher,
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-            {
-              matcher: { ...enforcerMatcher, namespace: diffNS },
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-          ],
-        },
-      } as Exemption;
-
-      processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
-
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedEnforcerMatcher,{
-          ...storedEnforcerMatcher,
-          namespace: diffNS,
-        }],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedEnforcerMatcher,{
-          ...storedEnforcerMatcher,
-          namespace: diffNS,
-        }],
-      );
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual(
-        [storedEnforcerMatcher,{
-          ...storedEnforcerMatcher,
-          namespace: diffNS,
-        }],
-      );
-    });
-
-    it("Adds duplicate exemptions set by same CR if different namespace and different policy list", async () => {
-      const diffNS = "differentNS";
-      const neuvectorMockExemption2 = {
-        metadata: {
-          uid: exemption1UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: enforcerMatcher,
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-            {
-              matcher: { ...enforcerMatcher, namespace: diffNS },
-              policies: [Policy.DisallowPrivileged],
-            },
-          ],
-        },
-      } as Exemption;
-
-      processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
-
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedEnforcerMatcher,{
-          ...storedEnforcerMatcher,
-          namespace: diffNS,
-        }],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual(
-        [storedEnforcerMatcher],
-      );
-    });
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([]);
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([
+      storedControllerMatcher,
+      storedPrometheusMatcher,
+    ]);
   });
 
-  describe("Test removeExemptions()", () => {
-    beforeEach(() => {
-       exemptionMap = new Map();
-       const policyList = Object.values(Policy);
-       for (const p of policyList) {
-         exemptionMap.set(p, []);
-       }
-    });
+  it("Removes matchers from policy if matchers removed from CR", async () => {
+    const updatedNeuvectorExemption = {
+      metadata: {
+        uid: exemption1UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: controllerMatcher,
+            policies: [Policy.DisallowPrivileged, Policy.DropAllCapabilities],
+          },
+          {
+            matcher: { ...enforcerMatcher, kind: MatcherKind.Service },
+            policies: [Policy.DisallowNodePortServices],
+          },
+        ],
+      },
+    } as Exemption;
 
-    it("Removes all CRs exemptions when deleted", async () => {
-      processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
-      processExemptions(neuvectorMockExemption, "DELETED", exemptionMap);
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([]);
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([]);
-    });
+    processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
+    processExemptions(updatedNeuvectorExemption, "MODIFIED", exemptionMap);
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([]);
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([storedControllerMatcher]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([storedControllerMatcher]);
+    expect(exemptionMap.get(Policy.DisallowNodePortServices)).toEqual([
+      { ...storedEnforcerMatcher, kind: MatcherKind.Service },
+    ]);
+  });
 
-    it("Does not remove exemptions set by separate CR from the one being deleted", async () => {
-      const promtailMockExemption = {
-        metadata: {
-          uid: exemption2UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: promtailMatcher,
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-          ],
-        },
-      } as Exemption;
+  it("Adds duplicate exemptions set by same CR if different matcher kind", async () => {
+    const neuvectorMockExemption2 = {
+      metadata: {
+        uid: exemption1UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: enforcerMatcher,
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+          {
+            matcher: { ...enforcerMatcher, kind: MatcherKind.Service },
+            policies: [Policy.DisallowNodePortServices],
+          },
+        ],
+      },
+    } as Exemption;
 
-      processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
-      processExemptions(promtailMockExemption, "ADDED", exemptionMap);
-      processExemptions(neuvectorMockExemption, "DELETED", exemptionMap);
+    processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
 
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedPromtailMatcher],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedPromtailMatcher],
-      );
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual(
-        [storedPromtailMatcher],
-      );
-    });
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.DisallowNodePortServices)).toEqual([
+      { ...storedEnforcerMatcher, kind: MatcherKind.Service },
+    ]);
+  });
 
-    it("Does not delete duplicate exemptions if set by separate CRs", async () => {
-      const neuvectorMockExemption2 = {
-        metadata: {
-          uid: exemption1UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: enforcerMatcher,
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-          ],
-        },
-      } as Exemption;
+  it("Adds duplicate exemptions set by same CR if different namespace", async () => {
+    const diffNS = "differentNS";
+    const neuvectorMockExemption2 = {
+      metadata: {
+        uid: exemption1UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: enforcerMatcher,
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+          {
+            matcher: { ...enforcerMatcher, namespace: diffNS },
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+        ],
+      },
+    } as Exemption;
 
-      const neuvectorDuplicatMockExemption = {
-        metadata: {
-          uid: exemption2UID,
-        },
-        spec: {
-          exemptions: [
-            {
-              matcher: enforcerMatcher,
-              policies: [
-                Policy.DisallowPrivileged,
-                Policy.DropAllCapabilities,
-                Policy.RequireNonRootUser,
-              ],
-            },
-          ],
-        },
-      } as Exemption;
+    processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
 
-      processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
-      processExemptions(neuvectorDuplicatMockExemption, "ADDED", exemptionMap);
-      processExemptions(neuvectorDuplicatMockExemption, "DELETED", exemptionMap);
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([
+      storedEnforcerMatcher,
+      {
+        ...storedEnforcerMatcher,
+        namespace: diffNS,
+      },
+    ]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([
+      storedEnforcerMatcher,
+      {
+        ...storedEnforcerMatcher,
+        namespace: diffNS,
+      },
+    ]);
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([
+      storedEnforcerMatcher,
+      {
+        ...storedEnforcerMatcher,
+        namespace: diffNS,
+      },
+    ]);
+  });
 
-      expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual(
-        [storedEnforcerMatcher],
-      );
-      expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual(
-        [storedEnforcerMatcher],
-      );
-    });
+  it("Adds duplicate exemptions set by same CR if different namespace and different policy list", async () => {
+    const diffNS = "differentNS";
+    const neuvectorMockExemption2 = {
+      metadata: {
+        uid: exemption1UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: enforcerMatcher,
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+          {
+            matcher: { ...enforcerMatcher, namespace: diffNS },
+            policies: [Policy.DisallowPrivileged],
+          },
+        ],
+      },
+    } as Exemption;
+
+    processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
+
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([
+      storedEnforcerMatcher,
+      {
+        ...storedEnforcerMatcher,
+        namespace: diffNS,
+      },
+    ]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([storedEnforcerMatcher]);
+  });
+});
+
+describe("Test removeExemptions()", () => {
+  beforeEach(() => {
+    exemptionMap = new Map();
+    const policyList = Object.values(Policy);
+    for (const p of policyList) {
+      exemptionMap.set(p, []);
+    }
+  });
+
+  it("Removes all CRs exemptions when deleted", async () => {
+    processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
+    processExemptions(neuvectorMockExemption, "DELETED", exemptionMap);
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([]);
+  });
+
+  it("Does not remove exemptions set by separate CR from the one being deleted", async () => {
+    const promtailMockExemption = {
+      metadata: {
+        uid: exemption2UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: promtailMatcher,
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+        ],
+      },
+    } as Exemption;
+
+    processExemptions(neuvectorMockExemption, "ADDED", exemptionMap);
+    processExemptions(promtailMockExemption, "ADDED", exemptionMap);
+    processExemptions(neuvectorMockExemption, "DELETED", exemptionMap);
+
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([storedPromtailMatcher]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([storedPromtailMatcher]);
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([storedPromtailMatcher]);
+  });
+
+  it("Does not delete duplicate exemptions if set by separate CRs", async () => {
+    const neuvectorMockExemption2 = {
+      metadata: {
+        uid: exemption1UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: enforcerMatcher,
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+        ],
+      },
+    } as Exemption;
+
+    const neuvectorDuplicatMockExemption = {
+      metadata: {
+        uid: exemption2UID,
+      },
+      spec: {
+        exemptions: [
+          {
+            matcher: enforcerMatcher,
+            policies: [
+              Policy.DisallowPrivileged,
+              Policy.DropAllCapabilities,
+              Policy.RequireNonRootUser,
+            ],
+          },
+        ],
+      },
+    } as Exemption;
+
+    processExemptions(neuvectorMockExemption2, "ADDED", exemptionMap);
+    processExemptions(neuvectorDuplicatMockExemption, "ADDED", exemptionMap);
+    processExemptions(neuvectorDuplicatMockExemption, "DELETED", exemptionMap);
+
+    expect(exemptionMap.get(Policy.DisallowPrivileged)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.DropAllCapabilities)).toEqual([storedEnforcerMatcher]);
+    expect(exemptionMap.get(Policy.RequireNonRootUser)).toEqual([storedEnforcerMatcher]);
+  });
 });
