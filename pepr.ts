c14e5ad676f84810e20813d9eac1404d34ae86bf
Micah Nagel <micah.nagel@defenseunicorns.com>
2024-06-05 11:29:23 -0600

    chore: peprstore cleanup

--- pepr.ts
+++ pepr.ts
@@ -4,6 +4,7 @@ import cfg from "./package.json";
 
 import { istio } from "./src/pepr/istio";
 import { operator } from "./src/pepr/operator";
+import { Policy } from "./src/pepr/operator/crd";
 import { registerCRDs } from "./src/pepr/operator/crd/register";
 import { policies, startExemptionWatch } from "./src/pepr/policies";
 import { prometheus } from "./src/pepr/prometheus";
@@ -26,6 +27,15 @@ import { prometheus } from "./src/pepr/prometheus";
     // Prometheus monitoring stack
     prometheus,
   ]);
+  // Remove legacy policy entries from the pepr store for the 0.5.0 upgrade
+  if (process.env.PEPR_WATCH_MODE === "true" && cfg.version === "0.5.0") {
+    Log.debug("Clearing legacy pepr store exemption entries...");
+    policies.Store.onReady(() => {
+      for (const p of Object.values(Policy)) {
+        policies.Store.removeItem(p);
+      }
+    });
+  }
 })().catch(err => {
   Log.error(err);
   process.exit(1);
