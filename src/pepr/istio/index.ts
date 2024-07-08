e64b47006bf48ae08a2d1a603376c8c11e10a1c9
UnicornChance <chance@defenseunicorns.com>
2024-07-08 15:54:30 -0600

    fix: drop pod info log

--- src/pepr/istio/index.ts
+++ src/pepr/istio/index.ts
@@ -24,10 +24,6 @@ When(a.Pod)
   .WithLabel("batch.kubernetes.io/job-name")
   .WithLabel("service.istio.io/canonical-name")
   .Watch(async pod => {
-    log.info(
-      pod,
-      `Processing Pod ${pod.metadata?.namespace}/${pod.metadata?.name} for istio job termination`,
-    );
 
     if (!pod.metadata?.name || !pod.metadata.namespace) {
       log.error(pod, `Invalid Pod definition`);
