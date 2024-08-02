d036ac5292e331ef53f19cfbee7b83ecc8c908c6
Wayne Starr <me@racer159.com>
2024-08-02 14:10:22 -0600

    lint

--- src/pepr/operator/crd/validators/package-validator.ts
+++ src/pepr/operator/crd/validators/package-validator.ts
@@ -104,15 +104,15 @@ export async function validator(req: PeprValidateRequest<UDSPackage>) {
     // If this is a public client ensure that it only sets itself up as an OAuth Device Flow client
     if (
       client.publicClient &&
-      (
-        (client.standardFlowEnabled === undefined || client.standardFlowEnabled) ||
-        (client.secret) ||
-        (client.secretName) ||
-        (client.secretTemplate) ||
-        (client.enableAuthserviceSelector) ||
-        (client.protocol === "saml") ||
-        (!client.attributes || client.attributes["oauth2.device.authorization.grant.enabled"] != "true")
-      )
+      (client.standardFlowEnabled === undefined ||
+        client.standardFlowEnabled ||
+        client.secret ||
+        client.secretName ||
+        client.secretTemplate ||
+        client.enableAuthserviceSelector ||
+        client.protocol === "saml" ||
+        !client.attributes ||
+        client.attributes["oauth2.device.authorization.grant.enabled"] != "true")
     ) {
       return req.Deny(
         `The client ID "${client.clientId}" must _only_ configure the OAuth Device Flow as a public client`,
