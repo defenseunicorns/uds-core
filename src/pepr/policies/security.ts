0e6f650e64fc501387e031ec0a5d39db78c45004
Noah Birrer <noah@defenseunicorns.com>
2024-08-23 13:57:08 -0400

    actually use `npx pepr format`

--- src/pepr/policies/security.ts
+++ src/pepr/policies/security.ts
@@ -34,7 +34,10 @@ When(a.Pod)
     // Check if any containers defined in the pod do not have the `allowPrivilegeEscalation` field present. If not, include it and set to false.
     for (const container of containers(request)) {
       container.securityContext = container.securityContext || {};
-      if (container.securityContext.allowPrivilegeEscalation === undefined && !container.securityContext.privileged) {
+      if (
+        container.securityContext.allowPrivilegeEscalation === undefined &&
+        !container.securityContext.privileged
+      ) {
         container.securityContext.allowPrivilegeEscalation = false;
       }
     }
