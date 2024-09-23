217772f3984a27763a30987b29b18fb4818d4aa5
Noah Birrer <noah@defenseunicorns.com>
2024-09-23 16:35:10 -0400

    correct tf outputs

--- .github/test-infra/aws/eks/outputs.tf
+++ .github/test-infra/aws/eks/outputs.tf
@@ -1,3 +1,27 @@
 output "region" {
   value = var.region
 }
+
+output "loki_irsa_role_arn" {
+  value = module.storage.loki_irsa_role_arn
+}
+
+output "loki_s3" {
+  value = module.storage.loki_s3
+}
+
+output "loki_s3_bucket" {
+  value = module.storage.loki_s3_bucket
+}
+
+output "velero_irsa_role_arn" {
+  value = module.storage.velero_irsa_role_arn
+}
+
+output "velero_s3" {
+  value = module.storage.velero_s3
+}
+
+output "velero_s3_bucket" {
+  value = module.storage.velero_s3_bucket
+}
