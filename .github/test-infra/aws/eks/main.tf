31dcd7cb6d0814904622e2858ba74131a635902f
Noah Birrer <noah@defenseunicorns.com>
2024-09-23 14:55:43 -0400

    pass `permissions_boundary_name` as input

--- .github/test-infra/aws/eks/main.tf
+++ .github/test-infra/aws/eks/main.tf
@@ -1,7 +1,8 @@
 # Prerequisites for EKS cluster that is provisioned later in this workflow via eksctl
 
 module "storage" {
-  source                   = "../modules/storage"
-  cluster_name             = var.name
-  use_permissions_boundary = var.use_permissions_boundary
+  source                    = "../modules/storage"
+  cluster_name              = var.name
+  use_permissions_boundary  = var.use_permissions_boundary
+  permissions_boundary_name = var.permissions_boundary_name
 }
