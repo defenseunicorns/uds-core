80a989202c62f81f6dd606e2ae01895678b69724
Wayne Starr <me@racer159.com>
2024-09-24 20:21:25 -0600

    feat: add frozen uds-core slim package

---
+++ packages/frozen-dev/README.md
@@ -0,0 +1,6 @@
+# UDS Core Frozen Dev
+
+This is a special modified version of UDS Core that rehydrates UDS Core Slim Dev from a committed container and volumes.
+
+> [!IMPORTANT]
+> In order to `zarf package create` this package you must have a running UDS k3d cluster with the UDS Core Slim dev installed in it!  This package also requires `sudo` to create and deploy currently - if you see a prompt and it seems stalled it is waiting for password input (hidden by the spinner)
---
+++ packages/frozen-dev/freeze.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Name of the running k3d container
+K3S_CONTAINER="k3d-uds-server-0"
+
+# Step 0: Ensure we can get sudo
+echo "This package requires elevated permissions to create - requesting sudo (if paused enter password)"
+sudo echo "got sudo! success!"
+
+# Step 1: Get the container ID of the running k3d container
+CONTAINER_ID=$(docker ps -qf "name=$K3S_CONTAINER")
+
+if [ -z "$CONTAINER_ID" ]; then
+    echo "No running container found for $K3S_CONTAINER"
+    exit 1
+fi
+
+# Step 2: Get the mounted volumes of the running container
+echo "Inspecting container volumes for $CONTAINER_ID..."
+VOLUMES=$(docker inspect -f '{{ json .Mounts }}' "$CONTAINER_ID" | jq)
+
+# Step 3: Prepare directories to save the volume data
+sudo rm -rf data
+mkdir -p data/kubelet_data data/k3s_data
+
+# Step 4: Loop through volumes and copy data to corresponding directories
+echo "Copying volumes to local directories..."
+
+for row in $(echo "$VOLUMES" | jq -r '.[] | @base64'); do
+    _jq() {
+        echo "${row}" | base64 --decode | jq -r "${1}"
+    }
+
+    SOURCE=$(_jq '.Source')
+    DESTINATION=$(_jq '.Destination')
+
+    case "$DESTINATION" in
+        "/var/lib/kubelet")
+            echo "Copying $SOURCE to data/kubelet_data/"
+            sudo cp -a "$SOURCE"/. data/kubelet_data/
+            ;;
+        "/var/lib/rancher/k3s")
+            echo "Copying $SOURCE to data/k3s_data/"
+            sudo cp -a "$SOURCE"/. data/k3s_data/
+            ;;
+        *)
+            echo "$DESTINATION is not needed. Skipping..."
+            ;;
+    esac
+done
+
+# Step 5: Commit and save the current container as a new image
+IMAGE_NAME="ghcr.io/defenseunicorns/uds-core/frozen:latest"
+echo "Committing container $CONTAINER_ID to image $IMAGE_NAME:latest..."
+docker commit -p "$CONTAINER_ID" "$IMAGE_NAME"
+
+echo "Saving image to data/uds-k3d-frozen-latest.tar..."
+docker save -o data/uds-k3d-frozen-latest.tar "$IMAGE_NAME"
+
+echo "Container image data/uds-k3d-frozen-latest.tar"
+
+# Step 6: Create a tarball from the data contents
+sudo tar -cpvf uds-frozen.tar data
+sudo chown -R $(whoami):$(whoami) "$(pwd)/uds-frozen.tar"
