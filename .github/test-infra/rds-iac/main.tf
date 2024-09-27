bb1813a646fdfd02606593bf43bd3b6c83836f1b
UnicornChance <chance@defenseunicorns.com>
2024-09-27 11:12:19 -0600

    fix: missing definition

--- .github/test-infra/rds-iac/main.tf
+++ .github/test-infra/rds-iac/main.tf
@@ -90,6 +90,10 @@ data "aws_subnets" "subnets" {
   }
 }
 
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 terraform {
   required_version = ">= 1.8.0"
   required_providers {
