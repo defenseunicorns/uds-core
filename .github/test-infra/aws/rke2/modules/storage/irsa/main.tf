05b302069ff7d98540d60de91f8d4501002287a1
Noah Birrer <noah@defenseunicorns.com>
2024-09-30 16:33:26 -0400

    add a module for rke2 s3 and irsa resources

---
+++ .github/test-infra/aws/rke2/modules/storage/irsa/main.tf
@@ -0,0 +1,29 @@
+data "aws_partition" "current" {}
+
+## This will create a policy for the S3 Buckets
+resource "aws_iam_policy" "s3_bucket_policy" {
+  name        = "${var.resource_prefix}${var.namespace}-policy"
+  path        = "/"
+  description = "IRSA policy to access buckets."
+  policy      = data.aws_iam_policy_document.s3_bucket_policy.json
+}
+
+## Create service account role
+resource "aws_iam_role" "s3_bucket_role" {
+  name                 = "${var.resource_prefix}${var.serviceaccount_name}-s3-role"
+  assume_role_policy   = data.aws_iam_policy_document.s3_bucket_role_policy.json
+  permissions_boundary = var.permissions_boundary
+
+  tags = {
+    PermissionsBoundary = split("/", var.permissions_boundary)[1]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy_attach" {
+  role       = "${var.resource_prefix}${var.serviceaccount_name}-s3-role"
+  policy_arn = aws_iam_policy.s3_bucket_policy.arn
+}
+
+resource "random_id" "unique_id" {
+  byte_length = 4
+}
