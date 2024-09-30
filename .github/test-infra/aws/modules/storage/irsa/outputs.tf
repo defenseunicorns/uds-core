e3120c7211e7605ff445b683812d4a06d2ec39ac
Noah Birrer <noah@defenseunicorns.com>
2024-09-30 16:27:32 -0400

    no longer use shared s3 module

--- .github/test-infra/aws/modules/storage/irsa/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bucket_role" {
-  value = aws_iam_role.s3_bucket_role
-}
--- .github/test-infra/aws/modules/storage/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_buckets" {
-  value = { for k, v in module.s3 : k => v }
-}
-
-output "irsa" {
-  value = { for k, v in module.irsa : k => v }
-}
