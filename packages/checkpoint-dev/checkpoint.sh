e34b0de44ffc19b0691785095b3cc1001b25b15b
Wayne Starr <me@racer159.com>
2024-10-01 17:40:41 -0600

    initial feedback

--- packages/checkpoint-dev/checkpoint.sh
+++ packages/checkpoint-dev/checkpoint.sh
@@ -3,6 +3,12 @@
 # Name of the running k3d container
 K3S_CONTAINER="k3d-uds-server-0"
 
+if [ -z "$TMPDIR" ]; then
+    #  macOS sets TMPDIR to a user temp directory - this also provides more options to linux
+    TMPDIR="/tmp"
+fi
+DATA_DIR="${TMPDIR}/uds-checkpoint-data"
+
 # Step 0: Ensure we can get sudo
 echo "This package requires elevated permissions to create - requesting sudo (if paused enter password)"
 sudo echo "got sudo! success!"
@@ -20,8 +26,8 @@ echo "Inspecting container volumes for $CONTAINER_ID..."
 VOLUMES=$(docker inspect -f '{{ json .Mounts }}' "$CONTAINER_ID" | jq)
 
 # Step 3: Prepare directories to save the volume data
-sudo rm -rf data
-mkdir -p data/kubelet_data data/k3s_data
+sudo rm -rf "$DATA_DIR"
+mkdir -p "${DATA_DIR}/kubelet_data" "${DATA_DIR}/k3s_data"
 
 # Step 4: Loop through volumes and copy data to corresponding directories
 echo "Copying volumes to local directories..."
@@ -36,12 +42,12 @@ for row in $(echo "$VOLUMES" | jq -r '.[] | @base64'); do
 
     case "$DESTINATION" in
         "/var/lib/kubelet")
-            echo "Copying $SOURCE to data/kubelet_data/"
-            sudo cp -a "$SOURCE"/. data/kubelet_data/
+            echo "Copying $SOURCE to ${DATA_DIR}/kubelet_data/"
+            sudo cp -a "$SOURCE"/. "${DATA_DIR}/kubelet_data/"
             ;;
         "/var/lib/rancher/k3s")
-            echo "Copying $SOURCE to data/k3s_data/"
-            sudo cp -a "$SOURCE"/. data/k3s_data/
+            echo "Copying $SOURCE to ${DATA_DIR}/k3s_data/"
+            sudo cp -a "$SOURCE"/. "${DATA_DIR}/k3s_data/"
             ;;
         *)
             echo "$DESTINATION is not needed. Skipping..."
@@ -54,15 +60,15 @@ IMAGE_NAME="ghcr.io/defenseunicorns/uds-core/checkpoint:latest"
 echo "Committing container $CONTAINER_ID to image $IMAGE_NAME:latest..."
 docker commit -p "$CONTAINER_ID" "$IMAGE_NAME"
 
-echo "Saving image to data/uds-k3d-checkpoint-latest.tar..."
-docker save -o data/uds-k3d-checkpoint-latest.tar "$IMAGE_NAME"
+echo "Saving image to ${DATA_DIR}/uds-k3d-checkpoint-latest.tar..."
+docker save -o "${DATA_DIR}/uds-k3d-checkpoint-latest.tar" "$IMAGE_NAME"
 
-echo "Container image saved to data/uds-k3d-checkpoint-latest.tar"
+echo "Container image saved to ${DATA_DIR}/uds-k3d-checkpoint-latest.tar"
 
 # Step 6: Create a tarball from the data contents
 echo "Creating a final tarball to include in the package"
-sudo tar --blocking-factor=64 -cpf uds-checkpoint.tar data
-sudo chown -R $(whoami):$(whoami) "$(pwd)/uds-checkpoint.tar"
+sudo tar --blocking-factor=64 -cpf uds-checkpoint.tar "$DATA_DIR"
+sudo chown -R $(id -u):$(id -g) "$(pwd)/uds-checkpoint.tar"
 
 echo "Successfully checkpointed the cluster!"
 
