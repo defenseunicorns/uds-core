2cc55f70f7432066e7372f707c8ff657acea40dc
Noah Birrer <noah@defenseunicorns.com>
2024-10-01 09:26:35 -0400

    dynamic name for statestore oidc bucket

--- .github/test-infra/aws/rke2/irsa.tf
+++ .github/test-infra/aws/rke2/irsa.tf
@@ -137,7 +137,7 @@ resource "random_password" "token" {
 
 module "statestore" {
   source = "./modules/statestore"
-  name   = local.cluster_name
+  name   = "${local.cluster_name}-${random_string.ssm.result}"
   token  = random_password.token.result
   tags   = local.tags
 }
