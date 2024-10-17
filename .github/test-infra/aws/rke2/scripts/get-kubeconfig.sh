a5af9d3936803dec6d68cf9c74cf7c14ec3c29ae
Noah Birrer <noah@defenseunicorns.com>
2024-10-17 11:26:08 -0400

    add debug output step to all distros

--- .github/test-infra/aws/rke2/scripts/get-kubeconfig.sh
+++ .github/test-infra/aws/rke2/scripts/get-kubeconfig.sh
@@ -32,7 +32,8 @@ scp -o StrictHostKeyChecking=no -i key.pem ${node_user}@${bootstrap_ip}:/home/${
 
 # Replace the loopback address with the cluster hostname
 sed -i "s/127.0.0.1/${bootstrap_ip}/g" ./rke2-config > /dev/null
-export KUBECONFIG=$(pwd)/rke2-config
+mv ./rke2-config /home/runnner/.kube/config
+#export KUBECONFIG=$(pwd)/rke2-config
 
 # find existing host record in the host file and save the line numbers
 matches_in_hosts="$(grep -n $cluster_hostname /etc/hosts | cut -f1 -d:)"
