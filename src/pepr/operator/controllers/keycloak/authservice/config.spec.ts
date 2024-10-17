6e923eddfe65b2cf662df9f4c140fe37ab64302c
UnicornChance <chance@defenseunicorns.com>
2024-10-17 14:10:31 -0600

    fix: license header

--- src/pepr/operator/controllers/keycloak/authservice/config.spec.ts
+++ src/pepr/operator/controllers/keycloak/authservice/config.spec.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import { afterEach, beforeEach, describe, expect, it, jest } from "@jest/globals";
 import { K8s, kind } from "pepr";
 import {
