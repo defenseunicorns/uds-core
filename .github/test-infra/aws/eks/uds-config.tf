c095284eb88fddeafc4ca6519294104a75b5d6d6
Noah Birrer <noah@defenseunicorns.com>
2024-10-18 08:24:44 -0400

    add license headers

--- .github/test-infra/aws/eks/uds-config.tf
+++ .github/test-infra/aws/eks/uds-config.tf
@@ -1,3 +1,6 @@
+# Copyright 2024 Defense Unicorns
+# SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+
 resource "local_sensitive_file" "uds_config" {
   filename = "../../../bundles/eks/uds-config.yaml"
   content = yamlencode({
