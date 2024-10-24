a17635fe8ad1588e5829db3ff0d1f77f3be1a126
UnicornChance <chance@defenseunicorns.com>
2024-10-24 07:59:41 -0600

    wip: jest testing

---
+++ e2e/tests/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
