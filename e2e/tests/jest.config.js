ead0e0ada917789dda9aee560a43ecab18635bf1
UnicornChance <chance@defenseunicorns.com>
2024-10-24 08:04:26 -0600

    fix: linting

--- e2e/tests/jest.config.js
+++ e2e/tests/jest.config.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
