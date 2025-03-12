de2e1dc50f72bfe35b7aa7b670f0efa50ed02e63
Noah Birrer <noah@defenseunicorns.com>
2025-03-12 09:46:42 -0400

    add sudo

--- .github/test-infra/aws/rke2/scripts/user_data.sh
+++ .github/test-infra/aws/rke2/scripts/user_data.sh
@@ -110,7 +110,7 @@ chmod +x yq
 #Fix for metrics server scraping of kubernetes api server components
 ./yq -i '.kube-controller-manager-arg[2] = "bind-address=0.0.0.0"' /etc/rancher/rke2/config.yaml
 ./yq -i '.kube-scheduler-arg += "bind-address=0.0.0.0"' /etc/rancher/rke2/config.yaml
-./yq -i '.listen-metrics-urls = "http://0.0.0.0:2381"' /var/lib/rancher/rke2/server/db/etcd/config
+sudo ./yq -i '.listen-metrics-urls = "http://0.0.0.0:2381"' /var/lib/rancher/rke2/server/db/etcd/config
 rm -rf ./yq
 }
 
