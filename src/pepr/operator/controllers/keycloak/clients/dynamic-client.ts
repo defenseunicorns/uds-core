71271883a7f64a565efe83dd0ed53f1b6b70c9c0
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2025-03-21 13:22:05 +0100

    Lint

--- src/pepr/operator/controllers/keycloak/clients/dynamic-client.ts
+++ src/pepr/operator/controllers/keycloak/clients/dynamic-client.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import { Client } from "../types";
 import {
   credentialsCreateOrUpdate,
