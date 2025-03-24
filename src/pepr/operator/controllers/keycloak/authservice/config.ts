7b974571a2b18667401f3dbea9ad27cebf3da290
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2025-03-24 10:33:00 +0100

    Fixed bootstrap error

--- src/pepr/operator/controllers/keycloak/authservice/config.ts
+++ src/pepr/operator/controllers/keycloak/authservice/config.ts
@@ -77,47 +77,49 @@ export async function setupAuthserviceSecret() {
 }
 
 export async function setupKeycloakClientSecret() {
-  // Ensure the namespace exists in the Kubernetes cluster
-  await K8s(kind.Namespace).Apply({
-    metadata: {
-      name: KEYCLOAK_CLIENTS_SECRET_NAMESPACE,
-    },
-  });
+  if (process.env.PEPR_WATCH_MODE === "true" || process.env.PEPR_MODE === "dev") {
+    // Ensure the namespace exists in the Kubernetes cluster
+    await K8s(kind.Namespace).Apply({
+      metadata: {
+        name: KEYCLOAK_CLIENTS_SECRET_NAMESPACE,
+      },
+    });
 
-  // Create the secret if it doesn't exist
-  try {
-    await K8s(kind.Secret)
-      .InNamespace(KEYCLOAK_CLIENTS_SECRET_NAMESPACE)
-      .Get(KEYCLOAK_CLIENTS_SECRET_NAME);
-    log.info(`Keycloak Clients Secret exists, skipping creation`);
-  } catch {
-    log.info("Keycloak Clients Secret does not exist, creating it");
+    // Create the secret if it doesn't exist
     try {
-      await K8s(kind.Secret).Apply(
-        {
-          metadata: {
-            namespace: KEYCLOAK_CLIENTS_SECRET_NAMESPACE,
-            name: KEYCLOAK_CLIENTS_SECRET_NAME,
+      await K8s(kind.Secret)
+        .InNamespace(KEYCLOAK_CLIENTS_SECRET_NAMESPACE)
+        .Get(KEYCLOAK_CLIENTS_SECRET_NAME);
+      log.info(`Keycloak Clients Secret exists, skipping creation`);
+    } catch {
+      log.info("Keycloak Clients Secret does not exist, creating it");
+      try {
+        await K8s(kind.Secret).Apply(
+          {
+            metadata: {
+              namespace: KEYCLOAK_CLIENTS_SECRET_NAMESPACE,
+              name: KEYCLOAK_CLIENTS_SECRET_NAME,
+            },
           },
-        },
-        { force: true },
-      );
-    } catch (err) {
-      log.error(err, "Failed to create Keycloak Clients Secret");
-      throw new Error("Keycloak Clients Secret.", { cause: err });
+          { force: true },
+        );
+      } catch (err) {
+        log.error(err, "Failed to create Keycloak Clients Secret");
+        throw new Error("Keycloak Clients Secret.", { cause: err });
+      }
     }
-  }
 
-  try {
-    const secret = await K8s(kind.Secret)
-      .InNamespace(KEYCLOAK_CLIENTS_SECRET_NAMESPACE)
-      .Get(KEYCLOAK_CLIENTS_SECRET_NAME);
-    await updateKeycloakClientsSecret(secret, false);
-  } catch (err) {
-    log.error(
-      err,
-      "Failed to rotate Keycloak Clients Secret. It might be an temporary error. Skipping",
-    );
+    try {
+      const secret = await K8s(kind.Secret)
+        .InNamespace(KEYCLOAK_CLIENTS_SECRET_NAMESPACE)
+        .Get(KEYCLOAK_CLIENTS_SECRET_NAME);
+      await updateKeycloakClientsSecret(secret, false);
+    } catch (err) {
+      log.error(
+        err,
+        "Failed to rotate Keycloak Clients Secret. It might be an temporary error. Skipping",
+      );
+    }
   }
 }
 
