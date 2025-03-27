4db9cc75aac1473ebd9cffd772e7be39761fa2a6
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2025-03-27 16:00:03 +0100

    feat: use Client Credentials for managing Keycloak Clients (#1341)
    ## Description
    
    This Pull Request introduces an alternative mode for managing Keycloak
    Clients by the UDS Operator.
    
    The UDS Operator now discovers if Keycloak has been properly configure
    (and if it supports the configuration delivered by
    https://github.com/defenseunicorns/uds-identity-config/pull/358) and
    switches to Client Credentials if it does. In other case, the UDS
    Operator will keep using the Dynamic Client Registration feature.
    
    The behavior might be overridden by using the
    `PEPR_KEYCLOAK_CLIENT_STRATEGY` Environment Variable in the UDS Operator
    that can have 3 values:
    
    * `dynamic_client_registration` - the Dynamic Client Registration mode
    * `client_credentials` - using the Client Credentials mode
    * `auto` - the Operator probes if a token obtained by the Client
    Credentials Grant looks good and picks proper mode according to the
    results of the check.
    
    The documentation for this feature will be delivered here:
    https://github.com/defenseunicorns/uds-core/pull/1367
    
    ## Related Issue
    
    * Fixes #1330
    * Relates to #1296
    * [Technical
    Design](https://www.notion.so/Managing-Keycloak-Clients-from-Pepr-1a5e512f24fc80c4bc35d13b9d7a1367)
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Steps to Validate
    
    This functionality primarily relies on the regression testing. 
    
    ## Checklist before merging
    
    - [x] Test, docs, adr added or updated as needed
    - [x] [Contributor
    Guide](https://github.com/defenseunicorns/uds-template-capability/blob/main/CONTRIBUTING.md)
    followed
    
    ---------
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
    Co-authored-by: Chance <139784371+UnicornChance@users.noreply.github.com>
    Co-authored-by: Noah <40781376+noahpb@users.noreply.github.com>
    Co-authored-by: Micah Nagel <micah.nagel@defenseunicorns.com>

---
+++ src/pepr/operator/controllers/keycloak/client-secret-sync.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2025 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
+import { K8s, kind } from "pepr";
+import { v4 as uuidv4 } from "uuid";
+import { Component, setupLogger } from "../../../logger";
+
+export const KEYCLOAK_CLIENT_SECRET_KEY = "uds-operator";
+
+export const KEYCLOAK_CLIENTS_SECRET_NAMESPACE = "keycloak";
+export const KEYCLOAK_CLIENTS_SECRET_NAME = "keycloak-client-secrets";
+
+const log = setupLogger(Component.OPERATOR_CONFIG);
+
+/**
+ * Updates the Keycloak client secret in the provided config.
+ * If the secret does not exist or forceRotation is true, a new secret is generated.
+ * The secret is then applied to the Kubernetes cluster.
+ *
+ * @param {kind.Secret} config - The Kubernetes Secret object to update.
+ * @param {boolean} [forceRotation=false] - Whether to force rotation of the secret.
+ */
+export async function updateKeycloakClientsSecret(
+  config: kind.Secret,
+  forceRotation: boolean = false,
+) {
+  config.data = config.data || {};
+
+  // This might be a bug but it seems Zarf adds managedFields, which is prohibited in Secrets.
+  delete config.metadata?.managedFields;
+
+  if (!config.data[KEYCLOAK_CLIENT_SECRET_KEY] || forceRotation) {
+    log.info("Generating new Keycloak client secret");
+    config.data[KEYCLOAK_CLIENT_SECRET_KEY] = Buffer.from(uuidv4()).toString("base64");
+    await K8s(kind.Secret).Apply(config);
+  }
+}
