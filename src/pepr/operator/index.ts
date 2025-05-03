c00e1bdf6bebdb91180c29605ea8edbae7c29fdf
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2025-05-03 09:24:44 +0200

    Moved annotation to the Service

--- src/pepr/operator/index.ts
+++ src/pepr/operator/index.ts
@@ -103,14 +103,6 @@ When(UDSPackage)
     UDSConfig.isIdentityDeployed = false;
   });
 
-// See https://istio.io/latest/docs/ambient/usage/waypoint/#useawaypoint
-When(a.Namespace)
-  .IsCreatedOrUpdated()
-  .WithName("keycloak")
-  .Mutate(req => {
-    req.SetLabel("istio.io/use-waypoint", "keycloak-waypoint");
-  });
-
 // Watch for changes to the Nodes and update the Node CIDR list
 if (!UDSConfig.kubeNodeCidrs) {
   When(a.Node).IsCreatedOrUpdated().Reconcile(updateKubeNodesFromCreateUpdate);
