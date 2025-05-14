af2ab0724422564828765f23cf187d3ab38cccfd
Case Wylie <cmwylie19@defenseunicorns.com>
2025-05-14 09:57:25 -0400

    chore: poc upgrade for uds-core
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "**/node_modules",
+    "**/dist",
+    "**/jest.*.js",
+    "test/playwright/",
+    "test/jest/",
+    "**/scripts/",
+    "**/uds-docs/",
+]), {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["./tsconfig.json"],
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-floating-promises": ["error"],
+    },
+}, {
+    files: [
+        "src/pepr/operator/crd/generated/**/*.ts",
+        "src/pepr/operator/crd/generated/*.ts",
+    ],
+
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+    },
+}]);
