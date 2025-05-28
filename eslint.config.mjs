3f401b8f24f7b3e7da7c314221118acdf2b61460
Case Wylie <cmwylie19@defenseunicorns.com>
2025-05-28 10:46:45 -0400

    chore: fix license
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
