81d580fa880bef545344f9d4714e309f63b4cec3
UnicornChance <chance@defenseunicorns.com>
2025-06-18 15:33:57 -0600

    fix: new velero validation of snapshots

--- .github/test-infra/aws/modules/irsa/main.tf
+++ .github/test-infra/aws/modules/irsa/main.tf
@@ -5,47 +5,8 @@ locals {
   irsa_role_name = try(coalesce(var.irsa_iam_role_name, format("%s-%s-%s", var.name, trim(var.kubernetes_service_account, "-*"), "irsa")), null)
 }
 
-resource "aws_iam_role" "this" {
-  name                  = local.irsa_role_name
-  description           = "AWS IAM Role for the Kubernetes service account ${var.kubernetes_service_account}."
-  assume_role_policy    = data.aws_iam_policy_document.this.json
-  max_session_duration  = 3600
-  permissions_boundary  = var.role_permissions_boundary_arn
-  force_detach_policies = true
-
-  tags = var.tags
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  for_each   = { for k, v in var.role_policy_arns : k => v }
-  role       = aws_iam_role.this.name
-  policy_arn = each.value
-}
-
-data "aws_iam_policy_document" "this" {
-
-  dynamic "statement" {
-    # https://aws.amazon.com/blogs/security/announcing-an-update-to-iam-role-trust-policy-behavior/
-    for_each = var.allow_self_assume_role ? [1] : []
-
-    content {
-      sid     = "ExplicitSelfRoleAssumption"
-      effect  = "Allow"
-      actions = ["sts:AssumeRole"]
-
-      principals {
-        type        = "AWS"
-        identifiers = ["*"]
-      }
-
-      condition {
-        test     = "ArnLike"
-        variable = "aws:PrincipalArn"
-        values   = ["arn:${var.current_partition}:iam::${var.account_id}:role/${var.irsa_iam_role_name}"]
-      }
-    }
-  }
-
+# TRUST POLICY
+data "aws_iam_policy_document" "trust_policy" {
   dynamic "statement" {
     for_each = var.oidc_providers
 
@@ -64,27 +25,47 @@ data "aws_iam_policy_document" "this" {
         values   = [for sa in statement.value.namespace_service_accounts : "system:serviceaccount:${sa}"]
       }
 
-      # https://aws.amazon.com/premiumsupport/knowledge-center/eks-troubleshoot-oidc-and-irsa/?nc1=h_ls
       condition {
         test     = "StringEquals"
         variable = "${replace(statement.value.provider_arn, "/^(.*provider/)/", "")}:aud"
         values   = ["sts.amazonaws.com"]
       }
-
     }
   }
+}
 
-  # velero aws plugin policy scope from here: https://github.com/vmware-tanzu/velero-plugin-for-aws?tab=readme-ov-file#set-permissions-for-velero
-  # ref policy for scoping based on tags: https://cloudonaut.io/restricting-access-to-ec2-instances-based-on-tags/
+# IAM ROLE
+resource "aws_iam_role" "this" {
+  name                  = local.irsa_role_name
+  description           = "AWS IAM Role for the Kubernetes service account ${var.kubernetes_service_account}."
+  assume_role_policy    = data.aws_iam_policy_document.trust_policy.json
+  max_session_duration  = 3600
+  permissions_boundary  = var.role_permissions_boundary_arn
+  force_detach_policies = true
+  tags                  = var.tags
+}
+
+# IAM ROLE POLICY ATTACHMENTS (external policies, like velero_policy)
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each   = { for k, v in var.role_policy_arns : k => v }
+  role       = aws_iam_role.this.name
+  policy_arn = each.value
+}
+
+# INLINE PERMISSIONS POLICY
+data "aws_iam_policy_document" "permissions_doc" {
+
+  # KMS access
   statement {
     effect = "Allow"
     actions = [
       "kms:ReEncryptFrom",
       "kms:ReEncryptTo"
     ]
-    resources = [data.aws_secretsmanager_secret_version.rke2_kms_key_arn_latest.secret_string]
+    resources = [var.kms_key_arn]
   }
 
+  # EC2 volume and snapshot access
   statement {
     effect = "Allow"
     actions = [
@@ -96,9 +77,7 @@ data "aws_iam_policy_document" "this" {
 
   statement {
     effect = "Allow"
-    actions = [
-      "ec2:CreateVolume",
-    ]
+    actions = ["ec2:CreateVolume"]
     resources = ["*"]
     condition {
       test     = "StringEquals"
@@ -109,9 +88,7 @@ data "aws_iam_policy_document" "this" {
 
   statement {
     effect = "Allow"
-    actions = [
-      "ec2:CreateSnapshot",
-    ]
+    actions = ["ec2:CreateSnapshot"]
     resources = ["*"]
     condition {
       test     = "StringEquals"
@@ -122,9 +99,7 @@ data "aws_iam_policy_document" "this" {
 
   statement {
     effect = "Allow"
-    actions = [
-      "ec2:CreateSnapshot",
-    ]
+    actions = ["ec2:CreateSnapshot"]
     resources = ["*"]
     condition {
       test     = "StringEquals"
@@ -135,9 +110,7 @@ data "aws_iam_policy_document" "this" {
 
   statement {
     effect = "Allow"
-    actions = [
-      "ec2:DeleteSnapshot"
-    ]
+    actions = ["ec2:DeleteSnapshot"]
     resources = ["*"]
     condition {
       test     = "StringEquals"
@@ -147,8 +120,8 @@ data "aws_iam_policy_document" "this" {
   }
 
   statement {
-    effect    = "Allow"
-    actions   = ["ec2:CreateTags"]
+    effect = "Allow"
+    actions = ["ec2:CreateTags"]
     resources = ["*"]
     condition {
       test     = "ForAllValues:StringEquals"
@@ -161,5 +134,11 @@ data "aws_iam_policy_document" "this" {
       values   = ["true"]
     }
   }
+}
 
+# Inline policy attachment
+resource "aws_iam_role_policy" "inline" {
+  name   = "${local.irsa_role_name}-inline"
+  role   = aws_iam_role.this.name
+  policy = data.aws_iam_policy_document.permissions_doc.json
 }
