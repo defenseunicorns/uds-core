01091ab3c13935ca81b0efbe76a1e8bdfdb5a5cd
Micah Nagel <micah.nagel@defenseunicorns.com>
2025-07-30 12:57:45 -0400

    chore: license

--- src/pepr/policies/common.spec.ts
+++ src/pepr/policies/common.spec.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import { K8s, kind } from "pepr";
 import { afterEach, beforeEach, describe, expect, it, Mock, vi } from "vitest";
 import { initPolicyVariables, parseImageRef, validateIstioImage } from "./common";
