d1529a887b459bddfff0ae7826df875825d6f06b
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2026-01-19 07:09:53 +0100

    feat: keycloak logout confirmation (#2260)
    ## Description
    
    This Pull Request leverages the new logout implementation from
    https://github.com/keycloak/keycloak/pull/44463 and sets logout
    confirmation to true by default.
    
    ## UDS Identity Config bug fix
    
    This Pull Request doesn't strictly require any UDS Identity Config
    updates but
    https://github.com/defenseunicorns/uds-identity-config/pull/768 fixes
    the following bug:
    
    <img width="953" height="423" alt="image"
    src="https://github.com/user-attachments/assets/a48c25a4-5b0c-4e8c-97de-01e1be0d5497"
    />
    
    
    ## Related Issue
    
    Fixes https://github.com/defenseunicorns/uds-identity-config/issues/689
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Steps to Validate
    
    Validated automatically in Grarana tests.
    
    The disabled confirmation code path is tested in UDS Identity Config
    
    ## Checklist before merging
    
    - [x] Test, docs, adr added or updated as needed
    - [x] [Contributor
    Guide](https://github.com/defenseunicorns/uds-template-capability/blob/main/CONTRIBUTING.md)
    followed

--- src/pepr/operator/controllers/keycloak/client-sync.spec.ts
+++ src/pepr/operator/controllers/keycloak/client-sync.spec.ts
@@ -191,7 +191,7 @@ describe("convertSsoToClient function", () => {
     const expectedClient: Partial<Client> = {
       clientId: "test-client",
       name: "Test Client",
-      attributes: { "uds.core.groups": "" },
+      attributes: { "uds.core.groups": "", "logout.confirmation.enabled": "true" },
     };
 
     expect(convertSsoToClient(sso)).toEqual(expectedClient);
@@ -228,6 +228,7 @@ describe("convertSsoToClient function", () => {
       attributes: {
         "backchannel.logout.revoke.offline.tokens": "true",
         "uds.core.groups": '{"anyOf":["group1"]}',
+        "logout.confirmation.enabled": "true",
       },
       defaultClientScopes: ["scope1", "scope2"],
       enabled: true,
@@ -254,7 +255,7 @@ describe("convertSsoToClient function", () => {
     const expectedClient: Partial<Client> = {
       clientId: "test-client",
       name: "Test Client",
-      attributes: { "uds.core.groups": '{"anyOf":[]}' },
+      attributes: { "uds.core.groups": '{"anyOf":[]}', "logout.confirmation.enabled": "true" },
       registrationAccessToken: undefined,
       samlIdpCertificate: undefined,
     };
@@ -272,7 +273,7 @@ describe("convertSsoToClient function", () => {
     const expectedClient: Partial<Client> = {
       clientId: "test-client",
       name: "Test Client",
-      attributes: { "uds.core.groups": "" },
+      attributes: { "uds.core.groups": "", "logout.confirmation.enabled": "true" },
     };
 
     expect(convertSsoToClient(sso)).toEqual(expectedClient);
@@ -309,6 +310,7 @@ describe("convertSsoToClient function", () => {
       attributes: {
         "backchannel.logout.revoke.offline.tokens": "true",
         "uds.core.groups": '{"anyOf":["group1","group2"]}',
+        "logout.confirmation.enabled": "true",
       },
       defaultClientScopes: ["scope1", "scope2"],
       enabled: true,
