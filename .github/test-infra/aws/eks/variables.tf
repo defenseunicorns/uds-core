c976e59b612a105186bb68a77e761bc211c4ab8a
Gabe <70963120+ntwkninja@users.noreply.github.com>
2026-09-10 08:48:58 -0500

    chore: upgrade eks to 1.36

--- .github/test-infra/aws/eks/variables.tf
+++ .github/test-infra/aws/eks/variables.tf
@@ -82,7 +82,7 @@ variable "databases" {
 variable "kubernetes_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.35"
+  default     = "1.36"
 }
 
 variable "vpc_name" {
